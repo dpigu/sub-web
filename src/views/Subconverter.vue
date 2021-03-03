5fdc561865a1d09286600f09bde7e1eea9e146a0
屁股 <joutlove@gmail.com>
2021-03-03 20:58:12 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -256,7 +256,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-	  "http://toss.tk (屁股专用）": "http://toss.tk/sub?",
+	  "toss.tk (屁股专用）": "http://toss.tk/sub?",
           "sub-beta.now.sh (自动编译最新版本后端-测试）": "https://sub-beta.now.sh/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
